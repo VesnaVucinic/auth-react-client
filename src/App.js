ce7859983de0e8e2eb3052805b5629586d12276d
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-27 00:21:50 +0000

    add state, handleLoginFormChange, handleLoginFormSubmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state ={
+      currentUser: null,
+      loginForm: {
+        email: "",
+        password: ""
+      }
+    }
+  }
+
+  handleLoginFormChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      loginForm: {
+        ...this.state.loginForm,
+        [name]: value
+      }
+    })
+  }
+
+  handleLoginFormSubmit = event =>{
+    event.preventDefault()
+    // now I need to submit info from the form to the backend
+    // where I will authanticate user and if valid send the user back
+    // with that response set my state
+    const userInfo = this.state.loginForm
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: userInfo
+      })
+    }
+    fetch("http://127.0.0.1:3001/login", headers)
+    .then(r => r.json())
+    .then(resp => {
+      if(resp.error) {
+        alert("Invalid credentials")
+      } else {
+        console.log(resp)
+        this.setState({
+          currentUser: resp
+          
+        })
+      }
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
+      <div className="App">
+          <h3>{ currentUser ? `Logged in as ${currentUser.name}` : "Not logged in"}</h3>
+          <Login
+            handleLoginFormChange={this.handleLoginFormChange}
+            handleLoginFormSubmit={this.handleLoginFormSubmit}
+            email={this.state.loginForm.email}
+            password={this.state.loginForm.password}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
